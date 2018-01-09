47710b9ceec6c0a12685178751f36788e4cb423e
jayesh <jawalejayesh1995@gmail.com>
2018-01-09 17:26:59 +0530

    modified index page of basic react setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Counter from './Counter';
-
-document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(
-    React.createElement(Counter),
-    document.getElementById('mount')
-  );
-});
+<html>
+<head>
+  <script src="/bundle.js" ></script>
+</head>
+<body>
+  Hello World
+</body>
+</html>
